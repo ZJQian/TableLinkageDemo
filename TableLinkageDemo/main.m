ab72a3d9b1f2fb54bb7c096d023c017a817e51ef
ZJQian <2326274522@qq.com>
2017-02-09 15:19:58 +0800

    Initial Commit

---
+++ TableLinkageDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableLinkageDemo
+//
+//  Created by ZJQ on 2017/2/9.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
